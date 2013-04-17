1cb8ac5a6ed42a777a35d923418347fb550fe434
John P. Bloch <john.bloch@10up.com>
2013-04-16 22:40:04 -0400

    Files

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,4 @@ namespace WPPerf;
 define( 'DIR', WP_PLUGIN_DIR . '/' . basename( __DIR__ ) );
 define( 'URL', WP_PLUGIN_URL . '/' . basename( __DIR__ ) );
 
+require_once DIR . '/functions/utilities.php';
