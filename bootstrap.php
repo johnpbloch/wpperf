e4a4212a755558f03597a9e347e8063a6269bf3f
John P. Bloch <john.bloch@10up.com>
2013-04-16 23:09:38 -0400

    A little more in the bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -6,3 +6,6 @@ define( 'DIR', WP_PLUGIN_DIR . '/' . basename( __DIR__ ) );
 define( 'URL', WP_PLUGIN_URL . '/' . basename( __DIR__ ) );
 
 require_once DIR . '/functions/utilities.php';
+require_once DIR . '/functions/core.php';
+
+add_action( 'plugins_loaded', n( 'setup' ) );
