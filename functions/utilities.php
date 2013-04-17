c296bb3866d079c6ae8aad109fc3ed01e963ed5a
John P. Bloch <john.bloch@10up.com>
2013-04-16 22:45:26 -0400

    Unit tests

---
+++ functions/utilities.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WPPerf;
+
+/**
+ * Prepends the WPPerf namespace to the front of function names
+ *
+ * @param string $function The function to be namespaced
+ *
+ * @return string The namespaced callback
+ */
+function n( $function ) {
+
+}
---
+++ tests/unit-tests/util.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WPPerf;
+
+use WP_Mock\Handler;
+
+require_once WPPERF_DIR . '/functions/utilities.php';
+
+class Utilities_Tests extends \PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+		\WP_Mock::setUp();
+		Handler::cleanup();
+	}
+
+	public function tearDown() {
+		\WP_Mock::tearDown();
+		Handler::cleanup();
+	}
+
+	public function test_n() {
+		$expected = '\WPPerf\foo';
+
+		$actual = \WPPerf\n( 'foo' );
+		$this->assertEquals(
+			$expected,
+			$actual,
+			'n() did not add the namespace as expected'
+		);
+
+		$actual = \WPPerf\n( '\foo' );
+		$this->assertEquals(
+			$expected,
+			$actual,
+			'n() did not strip leading backslashes as expected'
+		);
+	}
+
+}
